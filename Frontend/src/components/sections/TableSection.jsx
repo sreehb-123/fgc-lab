0b715fd6682a6f2893d16a2ed0f27dcbd8c933de
DhanushHN10 <dhanush.hn01@gmail.com>
2025-11-20 01:10:27 +0530

    Colour update for the string "Complete" as the user may provide any of "complete" or "completed".

--- Frontend/src/components/sections/TableSection.jsx
+++ Frontend/src/components/sections/TableSection.jsx
@@ -12,6 +12,7 @@ export default function TableSection({ data }) {
     "In Progress": "bg-yellow-100 text-yellow-800",
     Pending: "bg-blue-100 text-blue-800",
     Default: "bg-gray-100 text-gray-800",
+    Complete: "bg-green-100 text-green-800"
   };
 
   return (
